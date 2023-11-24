7e8a08af6809cac95978150f0a13db0b970b415a
optigo-app <130429763+optigo-app@users.noreply.github.com>
2023-11-24 13:31:09 +0530

    add data grid

--- src/views/SecondPage.js
+++ src/views/SecondPage.js
@@ -1,9 +1,49 @@
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useState } from "react";
 import { Card, CardHeader, CardBody, CardTitle, CardText } from "reactstrap";
 
 const SecondPage = () => {
+
+  const columns = [
+    { field: 'col1', headerName: 'Column 1', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
+  ];
+
+  const rows = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+
+  ];
+
+  const [currentCal, setCuurentcCal] = useState(columns);
+
+  const handleCallOrdr = (newColum) => {
+    setCuurentcCal(newColum);
+  }
+
   return (
-    <Card>
-      <CardHeader>
+    <div>
+      <div style={{ boxShadow: '0 4px 24px 0 rgba(34, 41, 47, 0.1)' }}>
+        <DataGrid
+          checkboxSelection={true}
+          pageSizeOptions={[5, 10, 15]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          rows={rows}
+          columns={currentCal}
+          onColumnOrderChange={handleCallOrdr}
+          components={{
+            Toolbar: GridToolbar
+          }}
+        />
+      </div>
+      {/* <CardHeader>
         <CardTitle>Create Data Grid 🙌</CardTitle>
       </CardHeader>
       <CardBody>
@@ -13,8 +53,8 @@ const SecondPage = () => {
           Carrot cake dragée chupa chups jujubes. Macaroon liquorice cookie
           wafer tart marzipan bonbon. Gingerbread jelly-o dragée chocolate.
         </CardText>
-      </CardBody>
-    </Card>
+      </CardBody> */}
+    </div>
   );
 };
 
